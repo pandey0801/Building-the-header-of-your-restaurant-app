05ab3d03a9af334a89c24c4937d7bc15159f3f46
vinay pandey <96349349+pandey0801@users.noreply.github.com>
2024-04-01 20:49:07 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./componenets/Layout/Header";
+import { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <h2>Let's get started!</h2>
+      <Header></Header>
+      </Fragment>
+    
+  );
+}
+
+export default App;
